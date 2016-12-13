dde197abb0d9ced44902bd29703246e750728058
farid7 <diraf_thechild@hotmail.com>
2016-12-13 12:45:13 -0600

    despliega datos, distribuciones aPriori

--- tarea4_despligaDatos/readingDataSet.R
+++ tarea4_despligaDatos/readingDataSet.R
@@ -6,30 +6,44 @@ library(ggplot2)
 library(DT)
 library(shiny)
 #setwd("/Users/LUSI_ITAM/Documents/farid/estadisticaComputacional_Clase/tareas/tarea4_setupReaingTable")
-setwd("/home/farid/Documents/estadistica computacional 2016b/tarea4_despligaDatos")
+#setwd("/home/farid/Documents/estadistica computacional 2016b/tarea4_despligaDatos")
 data <- read.csv(file="cheese.csv", header=T)
 
 ui <- fluidPage(
-  titlePanel("Basic DataTable"),
+  titlePanel("Tarea4_DataTable"),
+  h4("Angel Farid Fajardo Oroz"),
+  h5("MCC"),
   
   sidebarLayout(
     sidebarPanel(
      checkboxGroupInput("cVariables", h3("Variables"),
                               choices = names(data)),
      h4("Parámetros aPriori"),
-     sliderInput("a", "a", min=1, max=10, value=5),
-     sliderInput("sSigma", "sigma", min=1, max=10, value=5),
-     sliderInput("e", "error", min=1, max=10, value=5)
+     sliderInput("s_a", "a -> Unif ", min=1, max=10, value=c(5,8)),
+     sliderInput("s_b", "b <- Norm", min=1, max=10, value=5),
+     sliderInput("s_sigma", "sigma -> Unif", min=1, max=10, value=c(5, 6))
   ),
   
-  mainPanel("Tarea 4",
-  fluidRow(
-    splitLayout(cellWidths=c("50%", "50%"),DT::dataTableOutput("table"), plotOutput("Graph1"))
-    ),
-  plotOutput("gPriori")
-   )
+  mainPanel(
+    tabsetPanel(type = "tabs", 
+                tabPanel("datos",
+                  fluidRow(
+                  column(8, plotOutput("plot_data")),
+                  column(12, DT::dataTableOutput("table"))
+                   ) 
+                ),
+                tabPanel("distribuciones aPriori",
+                       fluidRow(
+                         column(4, plotOutput("plot_hist_A")),
+                         column(4, plotOutput("plot_hist_B")),
+                         column(4, plotOutput("plot_hist_Sd")),
+                         column(4, plotOutput("plot_hist_Total"))
+                       )
+                )
+               )
+   
   )
-)
+))
 
 server <- function(input, output) {
   n <- dim(data)[1]
@@ -41,10 +55,15 @@ server <- function(input, output) {
     aux
   })
   
-  priori <- reactive({
-    rnorm(n, mean=0, sd = input$sSigma)
+  priori_a <- reactive({
+    runif(n, min=input$s_a[1], max = input$s_a[2])
   })
-  
+  priori_b <- reactive({
+    rnorm(n, mean=0, sd = input$s_b)
+  })
+  priori_sd <- reactive({
+    runif(n, min=input$s_sigma[1], max = input$s_sigma[2])
+  })
   
   nmes <- renderText({
     input$cVariables
@@ -58,18 +77,31 @@ server <- function(input, output) {
      
   }))
    
-   output$Graph1 <- renderPlot({
+   output$plot_data <- renderPlot({
      if(is.null(input$cVariables))
        return()
      else{
        # aux1 <- dataInput()[,1]
        # aux2 <- dataInput()[,2]
-      return(plot(dataInput()))
+      return(plot(dataInput(), main="Grafica de dispersion para quesos"))
      }
    })
- output$gPriori <- renderPlot({
-   hist(priori(), title="Priori")
- }) 
+   
+  output$plot_hist_A <- renderPlot({
+    hist(priori_a())
+  })
+  
+  output$plot_hist_B <- renderPlot({
+    hist(priori_b())
+  })
+  
+  output$plot_hist_Sd <- renderPlot({
+    hist(priori_sd())
+  })
+  
+  output$plot_hist_Total <- renderPlot({
+    hist(priori_a() + priori_b() + priori_sd())
+  })
 }
 
 shinyApp(ui = ui, server = server)
